cce86354574fd9b860a7a29996641517a97e231d
william enrique <william21enrique@gmail.com>
2020-12-03 17:10:34 -0400

    integrar boton whatsapp

--- system/app/Views/Modules/footer.php
+++ system/app/Views/Modules/footer.php
@@ -9,11 +9,25 @@
 		</div>
 	</div>
 </footer><!-- End Footer -->
-
+	<div onclick="ht_ctc_click(this);"
+		class="ht-ctc ht-ctc-chat ctc_wp_desktop ctc-analytics style-2 ht_ctc_animation null"
+		style="position: fixed; bottom: 15px; left: 15px; cursor: pointer; z-index: 99999999;" data-return_type="chat"
+		data-style="2" data-number="56974930011" data-pre_filled="Hola, me interesan sus servicios (mensaje web)"
+		data-is_ga_enable="yes" data-is_fb_pixel="yes" data-is_fb_an_enable="no" data-webandapi="webapi"
+		data-display_mobile="show" data-display_desktop="show" data-css="display: none; cursor: pointer; z-index: 99999999;"
+		data-position="position: fixed; bottom: 15px; left: 15px;"
+		data-position_mobile="position: fixed; bottom: 10px; right: 10px;" data-show_effect="120" data-no_number="added">
+		<div style="display: flex; justify-content: center; align-items: center;  ">
+			<p class="ht-ctc-cta ht-ctc-cta-hover " style=" display: none;">
+				WhatsApp</p>
+			<img src="<?= IMG ?>whatsapp.svg" alt="" width="40" heith="40">
+		</div>
+	</div>
 <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 <div id="preloader"></div>
 <!-- Vendor JS Files -->
 <script src="<?= PLUGINS ?>jquery/jquery.min.js"></script>
+<script src="<?= PLUGINS ?>whatsapp/app.min.js"></script>
 <script src="<?= PLUGINS ?>bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="<?= PLUGINS ?>jquery.easing/jquery.easing.min.js"></script>
 <script src="<?= PLUGINS ?>php-email-form/validate.js"></script>
