b05d1f7239c6c1f98b76c2ad07a7fc5e0deeb33a
william enrique <william21enrique@gmail.com>
2021-01-13 16:37:49 -0400

    PHPMailer en local

--- system/core/Helpers/Helpers.php
+++ system/core/Helpers/Helpers.php
@@ -203,8 +203,7 @@ function email(string $nombre, string $email, string $asunto, string $mensaje){
 				$arrResponse = array("status" => false, "msg" => "Debe llenar los campos");
 	}else{
 
-
-		// $apl = 'infante';
+		$fecha = formatear_fecha(date('Y-m-d'));
 		$SES_USER = Username;
 		// configuro direccion de envio
 		$mail->setFrom($SES_USER, $nom);
@@ -219,48 +218,368 @@ function email(string $nombre, string $email, string $asunto, string $mensaje){
 		$mail->addReplyTo($destinatario);
 
 		// armo el body
-		// $body = $mensaje.' Enviar mensaje a '.$destinatario;
-		$message  = "<html><body>";
+		$message  = "<!DOCTYPE html	PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
 
-		$message .= "<table width='100%' bgcolor='#e0e0e0' cellpadding='0' cellspacing='0' border='0'>";
+<head>
+	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+	<title>Transporte&mudanzas</title>
+	<style type='text/css'>
+		body {
+			padding-top: 0 !important;
+			padding-bottom: 0 !important;
+			padding-top: 0 !important;
+			padding-bottom: 0 !important;
+			margin: 0 !important;
+			width: 100% !important;
+			-webkit-text-size-adjust: 100% !important;
+			-ms-text-size-adjust: 100% !important;
+			-webkit-font-smoothing: antialiased !important;
+		}
 
-		$message .= "<tr><td>";
+		.tableContent img {
+			border: 0 !important;
+			display: block !important;
+			outline: none !important;
+		}
 
-		$message .= "<table align='center' width='100%' border='0' cellpadding='0' cellspacing='0' style='max-width:650px; background-color:#fff; font-family:Verdana, Geneva, sans-serif;'>";
+		a {
+			color: #382F2E;
+		}
 
-		$message .= "<thead>
-			<tr height='80'>
-			<th colspan='4' style='background-color:#f5f5f5; border-bottom:solid 1px #bdbdbd; font-family:Verdana, Geneva, sans-serif; color:#333; font-size:34px;' >Mudanzas JCM</th>
+		p,
+		h1 {
+			color: #382F2E;
+			margin: 0;
+		}
+
+		p {
+			text-align: left;
+			color: #999999;
+			font-size: 14px;
+			font-weight: normal;
+			line-height: 19px;
+		}
+
+		a.link1 {
+			color: #382F2E;
+		}
+
+		a.link2 {
+			font-size: 16px;
+			text-decoration: none;
+			color: #ffffff;
+		}
+
+		h2 {
+			text-align: left;
+			color: #222222;
+			font-size: 19px;
+			font-weight: normal;
+		}
+
+		div,
+		p,
+		ul,
+		h1 {
+			margin: 0;
+		}
+
+		.bgBody {
+			background: #ffffff;
+		}
+
+		.bgItem {
+			background: #ffffff;
+		}
+
+		@media only screen and (max-width:480px) {
+
+			table[class='MainContainer'],
+			td[class='cell'] {
+				width: 100% !important;
+				height: auto !important;
+			}
+
+			td[class='specbundle'] {
+				width: 100% !important;
+				float: left !important;
+				font-size: 13px !important;
+				line-height: 17px !important;
+				display: block !important;
+				padding-bottom: 15px !important;
+			}
+
+			td[class='spechide'] {
+				display: none !important;
+			}
+
+			img[class='banner'] {
+				width: 100% !important;
+				height: auto !important;
+			}
+
+			td[class='left_pad'] {
+				padding-left: 15px !important;
+				padding-right: 15px !important;
+			}
+
+		}
+
+		@media only screen and (max-width:540px) {
+
+			table[class='MainContainer'],
+			td[class='cell'] {
+				width: 100% !important;
+				height: auto !important;
+			}
+
+			td[class='specbundle'] {
+				width: 100% !important;
+				float: left !important;
+				font-size: 13px !important;
+				line-height: 17px !important;
+				display: block !important;
+				padding-bottom: 15px !important;
+			}
+
+			td[class='spechide'] {
+				display: none !important;
+			}
+
+			img[class='banner'] {
+				width: 100% !important;
+				height: auto !important;
+			}
+
+			.font {
+				font-size: 18px !important;
+				line-height: 22px !important;
+
+			}
+
+			.font1 {
+				font-size: 18px !important;
+				line-height: 22px !important;
+
+			}
+		}
+	</style>
+
+	<script type='colorScheme' class='swatch active'>
+{
+    'name':'Default',
+    'bgBody':'ffffff',
+    'link':'382F2E',
+    'color':'999999',
+    'bgItem':'ffffff',
+    'title':'222222'
+}
+</script>
+
+</head>
+
+<body paddingwidth='0' paddingheight='0'
+	style='padding-top: 0; padding-bottom: 0; padding-top: 0; padding-bottom: 0; background-repeat: repeat; width: 100% !important; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased;'
+	offset='0' toppadding='0' leftpadding='0'>
+	<table bgcolor='#ffffff' width='100%' border='0' cellspacing='0' cellpadding='0' class='tableContent' align='center'
+		style='font-family:Helvetica, Arial,serif;'>
+		<tbody>
+			<tr>
+				<td>
+					<table width='600' border='0' cellspacing='0' cellpadding='0' align='center' bgcolor='#ffffff'
+						class='MainContainer'>
+						<tbody>
+							<tr>
+								<td>
+									<table width='100%' border='0' cellspacing='0' cellpadding='0'>
+										<tbody>
+											<tr>
+												<td valign='top' width='40'>&nbsp;</td>
+												<td>
+													<table width='100%' border='0' cellspacing='0' cellpadding='0'>
+														<tbody>
+															<!-- =============================== Header ====================================== -->
+															<tr>
+																<td height='75' class='spechide'></td>
+
+																<!-- =============================== Body ====================================== -->
+															</tr>
+															<tr>
+																<td class='movableContentContainer ' valign='top'>
+																	<div class='movableContent'
+																		style='border: 0px; padding-top: 0px; position: relative;'>
+																		<table width='100%' border='0' cellspacing='0' cellpadding='0'>
+																			<tbody>
+																				<tr>
+																					<td height='35'></td>
+																				</tr>
+																				<tr>
+																					<td>
+																						<table width='100%' border='0' cellspacing='0' cellpadding='0'>
+																							<tbody>
+																								<tr>
+																									<td valign='top' align='center' class='specbundle'>
+																										<div class='contentEditableContainer contentTextEditable'>
+																											<div class='contentEditable'>
+																												<p
+																													style='text-align:center;margin:0;font-family:Georgia,Time,sans-serif;font-size:26px;color:#222222;'>
+																													<span class='specbundle2'><span class='font1'>Transporte & Mudanzas</span></span></p>
+																											</div>
+																										</div>
+																									</td>
+																									<td valign='top' class='specbundle'>
+																										<div class='contentEditableContainer contentTextEditable'>
+																											<div class='contentEditable'>
+																												<p
+																													style='text-align:center;margin:0;font-family:Georgia,Time,sans-serif;font-size:26px;color:#1A54BA;'>
+																													<span class='font'></span> </p>
+																											</div>
+																										</div>
+																									</td>
+																								</tr>
+																							</tbody>
+																						</table>
+																					</td>
+																				</tr>
+																			</tbody>
+																		</table>
+																	</div>
+																	<div class='movableContent'
+																		style='border: 0px; padding-top: 0px; position: relative;'>
+																		<table width='100%' border='0' cellspacing='0' cellpadding='0' align='center'>
+																			<tr>
+																				<td valign='top' align='center'>
+																					<div class='contentEditableContainer contentImageEditable'>
+																						<div class='contentEditable'>
+																							<img src='".base_url()."src/images/line.png' width='251' height='43' alt=''
+																								data-default='placeholder' data-max-width='560'>
+																						</div>
+																					</div>
+																				</td>
+																			</tr>
+																		</table>
+																	</div>
+																	<div class='movableContent'
+																		style='border: 0px; padding-top: 0px; position: relative;'>
+																		<table width='100%' border='0' cellspacing='0' cellpadding='0' align='center'>
+																			<tr>
+																				<td height='55'></td>
+																			</tr>
+																			<tr>
+																				<td align='left'>
+																					<div class='contentEditableContainer contentTextEditable'>
+																						<div class='contentEditable' align='center'>
+																							<h2>Hola mi nombre es ".$nombre.".</h2>
+																						</div>
+																					</div>
+																				</td>
+																			</tr>
+
+																			<tr>
+																				<td height='15'> </td>
+																			</tr>
+
+																			<tr>
+																				<td align='left'>
+																					<div class='contentEditableContainer contentTextEditable'>
+																						<div class='contentEditable' align='center'>
+																							<p>".$mensaje."
+																								<br>
+																								<br>
+																								<span style='color:#222222;'>".$fecha."</span>
+																							</p>
+																						</div>
+																					</div>
+																				</td>
+																			</tr>
+
+																			<tr>
+																				<td height='55'></td>
+																			</tr>
+
+																			<tr>
+																				<td align='center'>
+
+																				</td>
+																			</tr>
+																			<tr>
+																				<td height='20'></td>
+																			</tr>
+																		</table>
+																	</div>
+																	<div class='movableContent'
+																		style='border: 0px; padding-top: 0px; position: relative;'>
+																		<table width='100%' border='0' cellspacing='0' cellpadding='0'>
+																			<tbody>
+																				<tr>
+																					<td height='65'>
+																				</tr>
+																				<tr>
+																					<td style='border-bottom:1px solid #DDDDDD;'></td>
+																				</tr>
+																				<tr>
+																					<td height='25'>
+																						<a href='#'class='color:#382F2E;'>Enviado por :".$nombre."</a><br>
+																						<a href'#'>Email :".$email."</a>
+																					</td>
+																				</tr>
+																				<tr>
+																					<td>
+																						<table width='100%' border='0' cellspacing='0' cellpadding='0'>
+																							<tbody>
+																								<tr>
+																									<td valign='top' class='specbundle'>
+																										<div class='contentEditableContainer contentTextEditable'>
+																											<div class='contentEditable' align='center'>
+																												<p
+																													style='text-align:left;color:#CCCCCC;font-size:12px;font-weight:normal;line-height:20px;'>
+																													<span style='font-weight:bold;'>Transporte & Mudanzas</span>
+																													<br>
+
+																												</p>
+																											</div>
+																										</div>
+																									</td>
+																									<td valign='top' width='30' class='specbundle'>&nbsp;</td>
+																									<td valign='top' class='specbundle'>
+
+																									</td>
+																								</tr>
+																							</tbody>
+																						</table>
+																					</td>
+																				</tr>
+																				<tr>
+																					<td height='88'></td>
+																				</tr>
+																			</tbody>
+																		</table>
+
+																	</div>
+
+																	<!-- =============================== footer ====================================== -->
+
+																</td>
+															</tr>
+														</tbody>
+													</table>
+												</td>
+												<td valign='top' width='40'>&nbsp;</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
 			</tr>
-								</thead>";
+		</tbody>
+	</table>
+</body>
 
-		$message .= "<tbody>
-									<tr align='center' height='50' style='font-family:Verdana, Geneva, sans-serif;'>
-										<td colspan='4' style='background-color:#00a2d1; text-align:center;'><a href='' style='color:#fff; text-decoration:none; text-transform : uppercase'>gracias por elejirnos</a></td>
-									</tr>
-
-									<tr>
-										<td colspan='4' style='padding:15px;'>
-											<p style='font-size:16px;'>Hola mi nombre es ' ".$nom." '.</p>
-											<hr />
-											<p style='font-size:15px; font-family:Verdana, Geneva, sans-serif;'>".$mensaje.".</p>
-											// <img src='https://williamenrique.github.io/mudanza/src/images/publicidad.jpg' alt='Sending HTML eMail using PHPMailer in PHP' title='Sending HTML eMail using PHPMailer in PHP' style='height:auto; width:100%; max-width:100%;' />
-											<p style='font-size:15px; font-family:Verdana, Geneva, sans-serif;'> Responder a ".$destinatario."</p>
-										</td>
-									</tr>
-
-									</tbody>";
-
-		$message .= "</table>";
-
-		$message .= "</td></tr>";
-		$message .= "</table>";
-
-		$message .= "</body></html>";
-
-		// message (esta parte no tengo clara para que espero si la quito funciona mal)
-		////$body = eregi_replace("[\]", '', $body);
-
+</html>";
 		// asigno el cuerpo a la clase
 		$mail->msgHTML($message);
 
@@ -271,23 +590,10 @@ function email(string $nombre, string $email, string $asunto, string $mensaje){
 
 		// Success
 		if ($mail->send()) {
-			$arrResponse = array("status" => true, "msg" => "Email enviado gracias por escibirnos, pronto seras contactado");
+			$arrResponse = array("status" => true, "msg" => "Gracias por comunicarte con nosotros ".$nombre." pronto seras contactado por nosotros.");
 		} else {
-			// $retorna['error'] = $mail->ErrorInfo;
 			$arrResponse = array("status" => false, "msg" => "A ocurrio un error en la configuración");
 		}
 	}
 	echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
 }
-
-// function email(string $nombre, string $email, string $asunto, string $mensaje){
-// 	$from="Mudanzas JCM  <soporte@mudanzajcm.com>";
-// 	$to = $email;
-// 	$subject = 'Estás a un solo click de participar' ;
-
-// 	$headers = 'From: ' . $from . PHP_EOL ;
-
-// 	$cuerpo ='';
-
-// 	mail($to, $subject, $cuerpo,"MIME-Version: 1.0\nContent-type: text/html; charset=UTF-8\n".$headers."");
-// }
